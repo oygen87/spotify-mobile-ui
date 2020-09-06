98601a095787f1b07c0ccf5378fed231fe8228f3
Eugen Cazacu <eugen.cazacu@cygni.se>
2020-09-06 20:40:47 +0200

    remove unused import, and format css

--- src/components/podcast/PodcastListItem.tsx
+++ src/components/podcast/PodcastListItem.tsx
@@ -1,5 +1,5 @@
 import * as React from "react";
-import { useContext, useEffect, useState } from "react";
+import { useContext, useState } from "react";
 import { PlayerContext } from "../../context/PlayerContext";
 import { Episode } from "../../types/types";
 import {
